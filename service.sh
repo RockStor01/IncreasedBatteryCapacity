83d25ead724a8180574e3d6924f362dcd6dec74a
RockStor01 <122721168+RockStor01@users.noreply.github.com>
2023-03-01 21:29:01 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,10 +1,10 @@
 #!/system/bin/sh
 #
-sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=4000000/' /sys/class/power_supply/bms/uevent
-sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' /sys/class/power_supply/bms/uevent
-sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=4000000/' /sys/class/power_supply/battery/uevent
-sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=4000000/' /sys/class/power_supply/battery/uevent
-echo 4000000 > /sys/class/power_supply/bms/charge_full
-echo 4000000 > /sys/class/power_supply/bms/charge_full_design
-echo 4000000 > /sys/class/power_supply/battery/charge_full
-echo 4000000 > /sys/class/power_supply/battery/charge_full_design
+sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=11500000/' /sys/class/power_supply/bms/uevent
+sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=11500000/' /sys/class/power_supply/bms/uevent
+sed -i 's/POWER_SUPPLY_CHARGE_FULL=.*./POWER_SUPPLY_CHARGE_FULL=11500000/' /sys/class/power_supply/battery/uevent
+sed -i 's/POWER_SUPPLY_CHARGE_FULL_DESIGN=.*./POWER_SUPPLY_CHARGE_FULL_DESIGN=11500000/' /sys/class/power_supply/battery/uevent
+echo 11500000 > /sys/class/power_supply/bms/charge_full
+echo 11500000 > /sys/class/power_supply/bms/charge_full_design
+echo 11500000 > /sys/class/power_supply/battery/charge_full
+echo 11500000 > /sys/class/power_supply/battery/charge_full_design
